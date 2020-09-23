81b5593f90074fe1d6f7790a7248b59d71f1e3ad
wanjochan <wanjochan@gmail.com>
2020-09-23 17:11:57 +0800

    tcctcc

--- tcctcc_build.sh
+++ tcctcc_build.sh
@@ -6,4 +6,6 @@ echo "#endif" >> tinycc/config.h
 #
 #./dtccgcc.sh tcc -static -nostdlib tinycc/tcc.c -o tcctcc tinycc/lib/libtcc1.o /w/alpine/crt1.o -lc
 #
-./dtccgcc.sh tcc -static -nostdlib tinycc/tcc.c -o tcctcc /usr/local/lib/tcc/libtcc1.a /usr/lib/crt1.o -lc
+#./dtccgcc.sh tcc -static -nostdlib tinycc/tcc.c -o tcctcc /usr/local/lib/tcc/libtcc1.a /usr/lib/crt1.o -lc
+./dtcctcc.sh tcc -static -nostdlib tinycc/tcc.c -o tcctcc /usr/local/lib/tcc/libtcc1.a /usr/lib/crt1.o -lc
+./tcctcc -vv
